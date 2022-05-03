10e2dc4ba5796091a5605ec7062c223d7a3f1335
ABrumby <a.brumby15@googlemail.com>
2022-05-04 00:31:32 +0100

    greet & IsPalindrome katas completed.

--- js/8kyu/Day1.js
+++ js/8kyu/Day1.js
@@ -11,4 +11,41 @@ function cockroachSpeed(s) {
     return result
 }
 
-export {reverseString, cockroachSpeed}
+//https://www.codewars.com/kata/577ff15ad648a14b780000e7
+function greet(language) {
+    if(language !== "" || language !== "IP_ADDRESS_INVALID" || language !== "IP_ADDRESS_NOT_FOUND" || language !== "IP_ADDRESS_REQUIRED") {
+        let result = languages.find(obj => {
+            return obj.language === language
+        })
+        if (result)
+            return result.greeting
+    }
+    return 'Welcome'
+}
+
+let languages = [
+    { language: 'english', greeting: 'Welcome'},
+    { language: 'czech', greeting: 'Vitejte'},
+    { language: 'danish', greeting: 'Velkomst'},
+    { language: 'dutch', greeting: 'Welkom'},
+    { language: 'estonian', greeting: 'Tere tulemast'},
+    { language: 'finnish', greeting: 'Tervetuloa'},
+    { language: 'flemish', greeting: 'Welgekomen'},
+    { language: 'french', greeting: 'Bienvenue'},
+    { language: 'german', greeting: 'Willkommen'},
+    { language: 'irish', greeting: 'Failte'},
+    { language: 'italian', greeting: 'Benvenuto'},
+    { language: 'latvian', greeting: 'Gaidits'},
+    { language: 'lithuanian', greeting: 'Laukiamas'},
+    { language: 'polish', greeting: 'Witamy'},
+    { language: 'spanish', greeting: 'Bienvenido'},
+    { language: 'swedish', greeting: 'Valkommen'},
+    { language: 'welsh', greeting: 'Croeso'}
+]
+
+//https://www.codewars.com/kata/57a1fd2ce298a731b20006a4
+function isPalindrome(x) {
+    return x.toLowerCase() === reverseString(x.toLowerCase())
+}
+
+export {reverseString, cockroachSpeed, greet, isPalindrome}
--- test/Day1.js
+++ test/Day1.js
@@ -1,9 +1,9 @@
 import { assert } from 'chai'
 import { expect } from 'chai'
-import {reverseString, cockroachSpeed} from '../js/8kyu/Day1.js'
+import {reverseString, cockroachSpeed, greet, isPalindrome} from '../js/8kyu/Day1.js'
 
 
-describe("Basic tests", () => {
+describe("Reverse string tests", () => {
     it("Testing for fixed tests", () => {
         assert.strictEqual(reverseString('world'), 'dlrow')
         assert.strictEqual(reverseString('hello'), 'olleh')
@@ -12,10 +12,29 @@ describe("Basic tests", () => {
     });
 });
 
-describe("Basic Tests", function() {
+describe("Cockroach Tests", function() {
     it("Testing for fixed tests", () => {
         assert.strictEqual(cockroachSpeed(1.08), 30);
         assert.strictEqual(cockroachSpeed(1.09), 30);
         assert.strictEqual(cockroachSpeed(0), 0);
     });
+});
+
+describe("Greeting Tests", () => {
+    it("test", () => {
+        assert.strictEqual(greet('english'), 'Welcome', "Your function should have returned 'Welcome'. Try again.");
+        assert.strictEqual(greet('dutch'), 'Welkom', "Your function should have returned 'Welkom'. Try again.");
+        assert.strictEqual(greet('IP_ADDRESS_INVALID'), 'Welcome', "Your function should have returned 'Welcome'. Try again.");
+    });
+});
+
+describe("isPalindrome Tests", function() {
+    it("Testing for 'a'", () => assert.strictEqual(isPalindrome("a"), true));
+    it("Testing for 'aba'", () => assert.strictEqual(isPalindrome("aba"), true));
+    it("Testing for 'Abba'", () => assert.strictEqual(isPalindrome("Abba"), true));
+    it("Testing for 'hello'", () => assert.strictEqual(isPalindrome("hello"), false));
+    it("Testing for 'Bob'", () => assert.strictEqual(isPalindrome("Bob"), true));
+    it("Testing for 'Madam'", () => assert.strictEqual(isPalindrome("Madam"), true));
+    it("Testing for 'AbBa'", () => assert.strictEqual(isPalindrome("AbBa"), true));
+    it("Testing for ''", () => assert.strictEqual(isPalindrome(""), true));
 });
