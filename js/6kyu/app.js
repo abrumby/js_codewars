41f6659672e5f1b462fc6bf75cb8b8037dd8e93e
ABrumby <a.brumby15@googlemail.com>
2022-05-04 02:14:25 +0100

    human readable slight refactor

--- js/6kyu/app.js
+++ js/6kyu/app.js
@@ -61,13 +61,12 @@ function humanReadable(seconds) {
 
     let minutes = Math.floor( seconds / 60 )
     seconds -= minutes * 60
-    if ( minutes < 10 ) {
-        minutes = '0' + minutes
-    }
 
-    if ( seconds < 10 ) {
+    if ( minutes < 10 )
+        minutes = '0' + minutes
+
+    if ( seconds < 10 )
         seconds = '0' + seconds
-    }
 
     return hours + ':' + minutes + ':' + seconds;
 }
